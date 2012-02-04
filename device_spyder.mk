7b67a523e34c5932fc736a351a3be4670654b832
Jason Theory <droidth3ory@gmail.com>
2012-02-03 22:44:21 -0500

    Fixed Line 271

--- device_spyder.mk
+++ device_spyder.mk
@@ -268,7 +268,7 @@ $(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
 #$(call inherit-product, hardware/ti/camera/camera.mk)
 $(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
 $(call inherit-product-if-exists, vendor/google/google-vendor.mk)
-$(call inherit-product-if-exists, vendor/verizon/spyder-verizon-vendor.mk)
+$(call inherit-product-if-exists, vendor/verizon/verizon-vendor.mk)
 
 $(call inherit-product-if-exists, vendor/motorola/spyder/spyder-vendor.mk)
 
